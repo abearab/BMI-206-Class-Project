0f433f07eaa87b6ab5e814f746e557b93e94d183
Isobel Beasley <isobel.beasley@ucsf.edu>
2024-10-28 15:40:32 -0700

    Updating filtering step of gwas summary stats to keep headers

--- code/gwas/filter_gwas_ss.sh
+++ code/gwas/filter_gwas_ss.sh
@@ -6,12 +6,12 @@
 # 92390283-99052124 liftover from hg19 to hg38
 # in hg38 88000001-93100000 ucsc browser
 
-fig_4a_data=("data/ra_uk_bb.h.filt.tsv"
+fig_4a_data=("output/gwas_ss/ra_uk_bb.h.filt.tsv"
                "data/t1d_uk_bb.h.filt.tsv"
                "data/hypo_uk_bb.h.filt.tsv"
                "data/finngen_atopic_derm.filt.tsv"
               )
-               
+              
 echo "Filtering Figure 4a datasets"
 for i in "${!fig_4a_data[@]}"; do
     gwas_sum_stat="${fig_4a_data[$i]}"
@@ -21,11 +21,11 @@ for i in "${!fig_4a_data[@]}"; do
     
     if [[ "$gwas_sum_stat" == *"uk"* ]]; then
        
-        awk -F'\t' '$3 == 6 && $4 >= 88000001 && $4 <= 93100000' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4a.tsv"
+        awk -F'\t' 'NR == 1 || $3 == 6 && $4 >= 88000001 && $4 <= 93100000' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4a.tsv"
     
     elif [[ "$gwas_sum_stat" == *"finngen"* ]]; then
        
-        awk -F'\t' '$1 == 6 && $2 >= 88000001 && $2 <= 93100000' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4a.tsv"
+        awk -F'\t' 'NR == 1 || $1 == 6 && $2 >= 88000001 && $2 <= 93100000' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4a.tsv"
     
     fi
 done 
@@ -47,7 +47,7 @@ for i in "${!fig_4b_data[@]}"; do
     echo " "
     echo "$gwas_sum_stat"
     
-    awk -F'\t' '$3 == 4 && $4 >= 21298378 && $4 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4b.tsv"
+    awk -F'\t' 'NR == 1 || $3 == 4 && $4 >= 21298378 && $4 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4b.tsv"
     
 done 
 
@@ -72,10 +72,10 @@ for i in "${!fig_4c_data[@]}"; do
     
     if [[ "$gwas_sum_stat" == *"age_meno"* ]]; then #menopause files have a different file structure
         
-        awk -F'\t' '$1 == 11 && $2 >= 21298378 && $2 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4c.tsv"
+        awk -F'\t' 'NR == 1 || $1 == 11 && $2 >= 21298378 && $2 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4c.tsv"
     else 
     
-       awk -F'\t' '$3 == 11 && $4 >= 21298378 && $4 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4c.tsv"
+       awk -F'\t' 'NR == 1 || $3 == 11 && $4 >= 21298378 && $4 <= 27698378' "$gwas_sum_stat" >  "${gwas_sum_stat%.tsv}.fig4c.tsv"
     fi
     
 done 
