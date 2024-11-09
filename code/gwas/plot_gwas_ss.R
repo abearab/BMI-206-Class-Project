ef548c60883e0f14b054bf2b38121717d9617744
Isobel Beasley <isobel.beasley@ucsf.edu>
2024-11-08 22:55:49 -0800

    Fixing plotting manhattans to look like paper

--- code/gwas/plot_gwas_ss.R
+++ code/gwas/plot_gwas_ss.R
@@ -8,15 +8,20 @@ make_plot_data = function(data_filename,
                           plot_chrom, 
                           plot_region_left, 
                          plot_region_right){
+  
 
-if (grepl("uk", data_filename) && !grepl("age_meno", data_filename)) {
+message("\n Making data for ", data_filename)
+  
+
+if (grepl("uk", data_filename) && 
+    !grepl("age_meno", data_filename)) {
   
   chrom_col <- "hm_chrom"
   pval_col <- "p_value"
   pos_col <- "hm_pos"
   rsid_col <- "hm_rsid"
   
-} else if (grepl("finn_gen", filename)){
+} else if (grepl("finngen", data_filename)){
   
   chrom_col <- "#chrom"
   pval_col <- "pval"
@@ -33,17 +38,14 @@ if (grepl("uk", data_filename) && !grepl("age_meno", data_filename)) {
 }
   
 data = data.table::fread(data_filename, 
-                  select = c(chrom_col= factor(),
-                             pval_col = numeric(),
-                             pos_col = integer(),
-                             rsid_col = character())
-                  # colClasses = c("factor",
-                  #                "numeric",
-                  #                "integer",
-                  #                "character")
+                  select = list(factor = chrom_col,
+                                numeric = pval_col,
+                                integer = pos_col,
+                                character = rsid_col)
                   )
 
-print(head(data))
+#print(head(data))
+
 # rename cols
 data = data |> 
        dplyr::rename(pos = !!pos_col,
@@ -52,35 +54,128 @@ data = data |>
                      rsid = !!rsid_col)
 
 
-print(head(data))
-
 # filtering correct genomic region 
 data = data |> 
   dplyr::filter(chrom == plot_chrom) #  #chromosomal region
 
+print(head(data))
+
 data = data |> # base regions
   dplyr::mutate(pos = pos / 10^6) |> 
-  dplyr::filter(pos >= plot_region_left & pos <= plot_region_right) # convert to mB - and filter proper figure region
+  dplyr::filter(pos >= plot_region_left - 1 & 
+                pos <= plot_region_right + 1) # convert to mB - and filter proper figure region
+
+#print(head(data))
 
 # 2. converting p-values for plotting 
 data = data |> 
+       dplyr::mutate(pval = as.numeric(pval))
+
+data = data |> 
        dplyr::mutate(log_pval = -1*log(pval, base = 10)) 
 
+#print(head(data))
+
 return(data)
+
 }
 
 
+plot_gg_manhattan = function(data){
+  
+  message("\n Make ggplot for ", data_filename)
+  
+  data |>
+    ggplot(aes(x=pos, y = log_pval)) +
+    geom_point(size = 0.5, alpha = 1) +
+    theme_bw() +
+    theme(
+      panel.grid.major = element_blank(),  # Remove major grid lines
+      panel.grid.minor = element_blank()   # Remove minor grid lines
+    ) + 
+    labs(x = "Position (chr 6, Mb)", 
+         y = expression(-log[10](P[GWAS])) 
+    )
+         #y = expression(paste0("-log_10(P_GWAS)")
+  
+  
+  plotfile = stringr::str_replace(data_filename,
+                                  pattern = ".filt.tsv",
+                                  replacement = ".png")
+  
+  plotfile = stringr::str_replace(plotfile,
+                                  pattern = "output/gwas_ss_filt",
+                                  replacement = "figures")
+  
+  
+  message("\n Saving plot as ", plotfile, "\n \n \n")
+  
+  
+  suppressMessages(ggsave(plotfile))
+}
+
+################ Figure 4a. ############# 
+
+
+fig_4a_data=c("output/gwas_ss_filt/ra_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/t1d_uk_bb.h.filt.tsv",
+              "output/gwas_ss_filt/hypo_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/finngen_atopic_derm.filt.tsv"
+           )
+
+#fig_4a_data = "output/gwas_ss_filt/finngen_atopic_derm.filt.tsv"
 # type 1 diabetes is looking correct 
 
-data = make_plot_data(data_filename = "output/gwas_ss_filt/ra_uk_bb.h.filt.tsv",
-                      plot_chrom = 6,
-                      plot_region_left = 90.1,
-                      plot_region_right = 90.4)
 
-data |>
-     ggplot(aes(x=pos, y = log_pval)) + 
-     geom_point(size = 0.75, alpha = 1) + 
-     theme_bw() + 
-     labs(x = "Position (chr 6, Mb)", y = "-log_10(P_GWAS)")
+for(data_filename in fig_4a_data){
+
+  data = make_plot_data(data_filename = data_filename,
+                        plot_chrom = 6,
+                        plot_region_left = 90.1,
+                        plot_region_right = 90.4)
+  
+  
+  plot_gg_manhattan(data)
+
+} 
 
 
+################# Figure 4b. ###################### 
+
+fig_4b_data=c("output/gwas_ss_filt/ra_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/t1d_uk_bb.h.filt.tsv"
+)
+
+for(data_filename in fig_4b_data){
+  
+  data = make_plot_data(data_filename = data_filename,
+                        plot_chrom = 4,
+                        plot_region_left = 26,
+                        plot_region_right = 26.20)
+  
+  
+  plot_gg_manhattan(data)
+  
+} 
+
+
+########### Figure 4c. ############ 
+
+
+fig_4c_data=c("output/gwas_ss_filt/endo_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/ovary_cys_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/menorrhagia_uk_bb.h.filt.tsv",
+             "output/gwas_ss_filt/age_meno_uk_bb.h.filt.tsv"
+)
+
+for(data_filename in fig_4c_data){
+  
+  data = make_plot_data(data_filename = data_filename,
+                        plot_chrom = 11,
+                        plot_region_left = 29.9,
+                        plot_region_right = 30.4)
+  
+  
+  plot_gg_manhattan(data)
+  
+} 
