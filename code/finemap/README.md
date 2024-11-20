5727fa3a4b5fe8041255685cda7e3e907ef39ed6
Isobel Beasley <isobel.beasley@ucsf.edu>
2024-11-20 13:31:43 -0800

    Restructure + better readmes for fine mapping and gwas summary statistics analysis

---
+++ code/finemap/README.md
@@ -0,0 +1,13 @@
+
+## Steps in fine mapped GWAS data processing for e.g. enrichment anaylsis 
+
+1. *download_finemap.sh* 
+
+Downloading and pre-processing steps of of FinnGen SUSIE fine-mapped datasets. 
+
+Input: 
+- Need gs utils to download required public data
+
+Output: 
+- Final combined file: output/fine_map/all_finngen_trait_cs_filtered.tsv
+- Subfiles (the files that make up the fine combined file): data/fine_map/*pip0.5.nonsyn
--- code/finemap/download_finemap.sh
+++ code/finemap/download_finemap.sh
@@ -1,33 +1,29 @@
 
-
+module load CBI
 module load miniforge3/24.7.1-0
+module load R/4.4.2
 conda activate bmi-206-group
 #conda install conda-forge::gsutil
 
-# get all the file names for the finemapping data v6
 
-gsutil cp gs://finngen-public-data-r6/finemapping/summaries/finngen_R6_ATOPIC_STRICT.SUSIE.snp.filter.tsv "test.v2.tsv"
+########### Part 1: Determine: what finngen files to dowload ##################
 
-#gs://finngen-public-data-r6/finemapping/full/finngen_R6_AB1_AMOEBIASIS.SUSIE.snp.bgz
+# a. Get all the file names for the finemapping data v6
 
-# so instead - let's extract those nanmes
-# for full SUSIE results 
-# gsutil ls gs://finngen-public-data-r6/finemapping/full |\
-#            grep '\.SUSIE\.snp\.bgz$' |\
-#            sed 's|.*/||' > data/fine_map/finngen_full/finngen_full_list.tsv
-
-# for snp-filter 
+# get a full list of avaliable snp-filter trait files
+# snp-filter susie datasets are 
 # snps that are part of good quality credible sets as reported in PHENONAME.cred.summary.tsv file.
 gsutil ls gs://finngen-public-data-r6/finemapping/summaries/ |\
            grep '\.SUSIE\.snp\.filter\.tsv$' |\
            sed 's|.*/||' > data/fine_map/finngen_snp_filter_list.tsv
 
 
-
-# these file names don't match what's avaliable in the bucket
+# b. the traits tested/file names don't exactly match what's avaliable in the bucket for the manifest file- 
+# so let's overlap them
 gsutil cp gs://finngen-public-data-r6/summary_stats/R6_manifest.tsv "data/fine_map/R6_manifest.tsv"
         
-           
+  
+# we overlap them while doing filtering step 1: filter for traits with cases > 1,000         
 RScript -e '
 
 R6_all = data.table::fread("data/fine_map/R6_manifest.tsv");
@@ -51,22 +47,38 @@ data.table::fwrite(matching_files,
                   )
 '
 
+########### Step 2. Download the files, and do basic processing ##############
+
 #module load htslib/1.21
 
 # download + basic process all the relevant files
 while read -r filename; do
 
   if [ ! -f "data/fine_map/$filename.pip0.5.nonsyn" ]; then
+  
       # Download each file from Google Cloud Storage and save it to data/fine_map
       gsutil cp gs://finngen-public-data-r6/finemapping/summaries/$filename "data/fine_map/$filename"
   
-  # # unzip file
-  # bgzip -d "data/fine_map/$filename"
-  # filename="${filename%.bgz}"
-  
-  # filter out snps with PIP <= 0.5
+      # Create .pip0.5 filtered files
+      # Filtering: remove snps with PIP <= 0.5
       awk 'NR == 1 {print; next} $5 > 0.5' "data/fine_map/$filename" > "data/fine_map/$filename.pip0.5"
 
+      # Create .pip0.5.nonsyn files
+      # Fltering: 
+      # include only: noncoding fine-mapped loci that did not include any nonsynonymous or splicing variants 
+      
+      # Currently this means filtering out the following tags: 
+      # missense_variant
+      # synonymous_variant
+      # frameshift_variant
+      # splice_acceptor_variant
+      # splice_donor_variant
+      # stop_gained
+      
+      # Not sure whether to also filter out: 
+      # ? non_coding_transcript_exon_variant
+      # ? intergenic_variant
+      
       awk '!($14 ~ /missense_variant|synonymous_variant|frameshift_variant|splice_acceptor_variant|splice_donor_variant|stop_gained/)' "data/fine_map/$filename.pip0.5" > "data/fine_map/$filename.pip0.5.nonsyn"
 
   #awk '$16 != -1' "data/fine_map/finngen_full/$filename" > "data/fine_map/finngen_full/$filename.cs"
@@ -77,11 +89,12 @@ rm "data/fine_map/$filename.pip0.5"
   
   done < data/fine_map/finngen_snp_filter_list.tsv
 
-# combine files if they contain at least one snp 
+
+################### Step 3: Combine filtered files if they contain at least one snp ##############
 
 
 # Create an empty list to hold filenames
-output_file="data/fine_map/all_finngen_trait_cs_filtered.tsv"
+output_file="output/fine_map/all_finngen_trait_cs_filtered.tsv"
 
 > "$output_file"  # Initialize the output file (clear it if it exists)
 
@@ -96,36 +109,3 @@ for file in data/fine_map/*.pip0.5.nonsyn; do
       echo "" >> "$output"  # Add a newline after each file for separation
   fi
 done
-
-# Filtering: step 1
-
-#  selected any traits with case count >1,000
-
-
-# Filtering: step 2: 
-
-# noncoding fine-mapped loci that did not include any nonsynonymous or splicing variants 
-
-# missense_variant
-# synonymous_variant
-# frameshift_variant
-# splice_acceptor_variant
-# splice_donor_variant
-# stop_gained
-
-# ? non_coding_transcript_exon_variant
-# ? intergenic_variant
-
-
-# fingenn_manifest = data.table::fread("data/fine_map/R6_manifest.tsv")
-# finngenn_cred_filenames = data.table::fread("finngen_filename_cred_sum.tsv")
-# 
-# finngenn_cred_filenames = stringr::remove_string("finngen_R6_", finngenn_cred_filenames)
-# finngenn_cred_filenames = stringr::remove_string("SUSie")
-
-# data/fine_
-# finngen_R6_C3_
-
-# gsutil ls gs://finngen-public-data-r6/finemapping/summaries/ |\
-#            grep '\.SUSIE\.snp\.filter\.tsv$' |\
-#            sed 's|.*/||' > data/fine_map/finngen_cred_sum.tsv
