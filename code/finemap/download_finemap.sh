a150cae518dfbb20bc39843f36b3d592bbe6e1ac
abearab <abolfazl.arab@ucsf.edu>
2024-11-21 01:08:20 -0800

    minor fix
    related to https://github.com/abearab/BMI-206-Class-Project/issues/2#issuecomment-2490417072

--- code/finemap/download_finemap.sh
+++ code/finemap/download_finemap.sh
@@ -1,11 +1,4 @@
-
-# module load CBI
-# module load miniforge3/24.7.1-0
-# module load R/4.4.2
-# conda activate bmi-206-group
-# #conda install conda-forge::gsutil
-
-
+#conda activate bmi-206-group
 mkdir -p output/fine_map
 
 ########### Part 1: Determine: what finngen files to dowload ##################
@@ -96,18 +89,12 @@ rm "data/fine_map/$filename.pip0.5"
 
 
 # Create an empty list to hold filenames
-output_file="output/fine_map/all_finngen_trait_cs_filtered.tsv"
-
-> "$output_file"  # Initialize the output file (clear it if it exists)
-
-# Loop through each .snp.filter.tsv file in the folder
-
-for file in data/fine_map/*.pip0.5.nonsyn; do
-  # Check if the file has more than 1 line
-  line_count=$(wc -l < "$file")
-  
-  if [ "$line_count" -gt 1 ]; then
-      cat "$file" >> "$output_file"
-      echo "" >> "$output_file"  # Add a newline after each file for separation
-  fi
-done
+python -c """
+from glob import glob
+import pandas as pd
+pd.concat([
+  pd.read_csv(file,sep='\t',header=0) 
+  for file in glob('data/fine_map/*nonsyn')
+  if pd.read_csv(file,sep='\t',header=0).shape[0] > 0
+]).reset_index(drop=True).to_csv('output/fine_map/all_finngen_trait_cs_filtered.tsv', sep='\t', index=False)
+"""
